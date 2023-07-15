491f73659153e3963cd7dac48c0a1741856dc200
Roshni jeewani <89697445+Roshnijeewani3457@users.noreply.github.com>
2023-07-15 17:01:57 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -14,7 +14,7 @@ const getBackground=()=>{
    {
    
     document.body.style.backgroundImage = "url('Images/morning.jpg')";
-    document.body.style.backgroundSize="1700px 900px"
+    document.body.style.backgroundSize="1700px 1200px"
   
    }
    else
@@ -34,7 +34,7 @@ const getBackground=()=>{
    if(afterNoon=true)
    {
     document.body.style.backgroundImage = "url('Images/morning.jpg')";
-    document.body.style.backgroundSize="1700px 700px"
+    document.body.style.backgroundSize="1700px 1200px"
    }
    
 
@@ -52,6 +52,8 @@ function(event)
     {
     weather.innerHTML=``;
     weather.style=``; 
+    location.reload();
+    
     }
     
     getWeather(search.value);
@@ -102,7 +104,8 @@ weather.style.padding="34px 34px";
 weather.style.margin="19px";
 weather.style.height="320px";
 weather.style.background="white";
-
+const m=document.querySelector('#map');
+m.style.display = 'none';
 
 }
 
@@ -121,11 +124,8 @@ const findMyState= ()=>{
                 console.log(data);
                 search.value=data.city
                 search.focus();
-             })
-
-
-
-    }
+            })
+}
     const error = () =>{
         status.textContent="Unable to retrieve location";
     }
@@ -135,36 +135,79 @@ const findMyState= ()=>{
       } else {
         console.log("Geolocation is not supported by this browser.");
       }
-  
+      
 
     
     }
-    function handlePermission() {
-        navigator.permissions.query({ name: "geolocation" }).then((result) => {
-          if (result.state === "granted") {
-            report(result.state);
-            geoBtn.style.display = "none";
-          } else if (result.state === "prompt") {
-            report(result.state);
-            geoBtn.style.display = "none";
-            navigator.geolocation.getCurrentPosition(
-              revealPosition,
-              positionDenied,
-              geoSettings,
-            );
-          } else if (result.state === "denied") {
-            report(result.state);
-            geoBtn.style.display = "inline";
-          }
-          result.addEventListener("change", () => {
-            report(result.state);
-          });
-        });
+    var map = L.map('map').setView([20.5937, 78.9629], 5);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  
+   var marker;
+
+    map.on('click', function(e) {
+      var lat = e.latlng.lat;
+      var lng = e.latlng.lng;
+  
+      if (marker) {
+        map.removeLayer(marker); // Remove previous marker if it exists
       }
-      
-      function report(state) {
-        console.log(`Permission ${state}`);
-      }
-      
-      handlePermission();
-      
+  
+      marker = L.marker([lat, lng]).addTo(map);
+  
+      // Fetch weather data for the clicked location
+      fetchWeatherData(lat, lng)
+  })
+  function fetchWeatherData(lat, lng) {
+    // Replace 'YOUR_API_KEY' with your actual API key
+    var apiKey = `3265874a2c77ae4a04bb96236a642d2f`
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}`;
+
+    
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+           <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+        <div>
+        <br>
+        <h4>Maximum Temp- ${data.main.temp_max}℃</h4>
+        <h4>Minimum Temp- ${data.main.temp_min}℃</h4>
+        </div>
+        <div>
+        <h4>Wind speed- ${data.wind.speed}km/h </h4>
+         <h4>Humidity- ${data.main.humidity}% </h4>
+         <h4>Sunset- ${data.main.pressure}mb </h4>
+     </div>
+        </div>
+        </div>
+    `
+    weather.style.border="2px solid brown";
+    weather.style.color="brown";
+    weather.style.padding="34px 34px";
+    weather.style.margin="19px";
+    weather.style.height="320px";
+    weather.style.background="white";
+    const m=document.querySelector('#map');
+    m.style.display = 'none';
+    
+  })
+      .catch(error => {
+        console.log('Error fetching weather data:', error);
+      });
+  }
+  function refreshPage() {
+    location.reload();
+  }
+  
+
+  
